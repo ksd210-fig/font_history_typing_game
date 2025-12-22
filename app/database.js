48395cf40a53a67708ae890a8215d914183d9bad
fig1 <ksd210@gmail.com>
2025-12-22 23:19:15 +0900

    기본 로직 제작

---
+++ app/database.js
@@ -0,0 +1,16 @@
+export const Data = [
+    {
+      id: 1,
+      name: "Helvetica",
+      year: 1957,
+      designer: "Max Miedinger",
+      history: "스위스에서 탄생한 네오 그로테스크 산세리프 서체",
+    },
+    {
+      id: 2,
+      name: "Times New Roman",
+      year: 1931,
+      designer: "Stanley Morison",
+      history: "The Times 신문을 위해 제작된 세리프 서체",
+    },
+  ];
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,41 @@
+"use client";
+
+import { useState } from "react";
+import { Data } from "./database";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  const [typedText, setTypedText] = useState("");
+  const originalText = Data[0].history;
+
+  return (
+    <div className="w-[800px] h-[1280px] mx-auto flex items-center justify-center">
+      <div className="relative">
+        {/* 원본 텍스트 (아래 레이어) */}
+        <p className="text-gray-500">{originalText}</p>
+
+        {/* 입력된 텍스트 */}
+        <p className="absolute top-0 left-0">
+          {typedText.split("").map((char, index) => (
+            <span
+              key={index}
+              className={
+                char === originalText[index] ? "text-white" : "text-red-500"
+              }
+            >
+              {char}
+            </span>
+          ))}
+        </p>
+
+        {/* 숨겨진 입력창 */}
+        <input
+          type="text"
+          value={typedText}
+          onChange={(e) => setTypedText(e.target.value)}
+          autoFocus
+          className="absolute top-0 left-0 w-full opacity-0"
+        />
+      </div>
+    </div>
+  );
 }
